b502ac6576c050ec99c879654cedcae4e5528826
qrzj <1774878535@qq.com>
2020-08-30 20:01:26 +0800

    修改了package.json

---
+++ build/precache-manifest.e1c9174ee6d9eda60e7aecae677a8db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d396cc519ef45c60d3fa7426bf970348",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bd5e73fcda40716a0ca",
+    "url": "./static/css/2.fb92a525.chunk.css"
+  },
+  {
+    "revision": "935c26a341c8572d7fd8",
+    "url": "./static/css/main.e402881e.chunk.css"
+  },
+  {
+    "revision": "5bd5e73fcda40716a0ca",
+    "url": "./static/js/2.285dc556.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.285dc556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935c26a341c8572d7fd8",
+    "url": "./static/js/main.059dc330.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
